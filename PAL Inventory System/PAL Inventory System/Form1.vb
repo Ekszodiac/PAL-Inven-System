351cee4d70c0e6da6616f42c7153d6fc5c2b7921
ekszodiac <Ekszodiac@gmail.com>
2018-07-10 22:28:07 -0800

    1st Upload

---
+++ PAL Inventory System/PAL Inventory System/Form1.vb
@@ -0,0 +1,13 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.WindowState = FormWindowState.Maximized
+        Me.BackColor = Color.FromArgb(0, 31, 96)
+        loginbut.BackColor = Color.FromArgb(255, 201, 7)
+        loginbut.FlatAppearance.BorderSize = 1
+    End Sub
+
+    
+End Class
