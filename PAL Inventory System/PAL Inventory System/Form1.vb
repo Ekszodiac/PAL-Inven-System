279fa36ec7a8c02599f1b211658ead449d1e90ee
ekszodiac <Ekszodiac@gmail.com>
2018-07-15 22:13:03 -0800

    3rd Push

--- PAL Inventory System/PAL Inventory System/Form1.vb
+++ PAL Inventory System/PAL Inventory System/Form1.vb
@@ -1,6 +1,7 @@
 ﻿Imports System.IO
 
 Public Class Form1
+    Public SQL As New SQLControl
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Me.WindowState = FormWindowState.Maximized
---
+++ PAL Inventory System/PAL Inventory System/SQLControl.vb
@@ -0,0 +1,58 @@
+﻿Imports System.Data.SqlClient
+
+Public Class SQLControl
+    Private DBCon As New SqlConnection("Server=RVO;Database=InvenSystem;User=RVO;")
+    Private DBCmd As SqlCommand
+
+    Public DBDA As SqlDataAdapter
+    Public DBDT As DataTable
+
+    Public Params As New List(Of SqlParameter)
+
+    Public RecordCount As Integer
+    Public Exception As String
+
+    Public Sub New()
+
+    End Sub
+
+    Public Sub New(ConnectionString As String)
+        DBCon = New SqlConnection(ConnectionString)
+    End Sub
+
+    Public Sub ExecQuery(Query As String)
+        RecordCount = 0
+        Exception = ""
+
+        Try
+            DBCon.Open()
+
+            DBCmd = New SqlCommand(Query, DBCon)
+
+            Params.ForEach(Sub(p) DBCmd.Parameters.Add(p))
+
+            Params.Clear()
+
+            DBDT = New DataTable
+            DBDA = New SqlDataAdapter(DBCmd)
+            RecordCount = DBDA.Fill(DBDT)
+
+        Catch ex As Exception
+            Exception = "ExecQuery Error " & vbNewLine & ex.Message
+        Finally
+            'CLOSE CONNECTION
+            If DBCon.State = ConnectionState.Open Then DBCon.Close()
+        End Try
+    End Sub
+
+    Public Sub AddParam(Name As String, Value As Object)
+        Dim NewParam As New SqlParameter(Name, Value)
+        Params.Add(NewParam)
+    End Sub
+
+    Public Function HasException(Optional Report As Boolean = False) As Boolean
+        If String.IsNullOrEmpty(Exception) Then Return False
+        If Report = True Then MsgBox(Exception, MsgBoxStyle.Critical, "Exception:")
+        Return True
+    End Function
+End Class
